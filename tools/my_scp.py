b5ad787ab8f96e2bba983f83abe13ade0d5930cd
uloveqian2021 <88141320+uloveqian2021@users.noreply.github.com>
2021-07-29 19:10:08 +0800

    Update my_scp.py

--- tools/my_scp.py
+++ tools/my_scp.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 """
-@author : su shaoxun
+@author : 
 @time   :20-7-21 上午11:18 
 @IDE    :PyCharm
 @document   :1.py
@@ -13,20 +13,16 @@ import os
 # 将指定目录的图片文件上传到服务器指定目录
 # remote_path远程服务器目录
 # file_path本地文件夹路径
-# img_name是file_path本地文件夹路径下面的文件名称 sushaoxunspace/resultdata
+# img_name是file_path本地文件夹路径下面的文件名称
 
 
 def upload_file(remote_path="/wang/save_model/ccks2020ee/", file_path="./README.md", is_file=False):
     # img_name示例：07670ff76fc14ab496b0dd411a33ac95-6.webp
-    # host = "10.10.150.91"  # 服务器ip地址
-    # port = 22  # 端口号
-    # username = "root"  # ssh 用户名
-    # password = "bd@2020"  # 密码
 
-    host = "10.10.177.28"  # 服务器ip地址
+    host = ""  # 服务器ip地址
     port = 22  # 端口号
-    username = "pubuser"  # ssh 用户名
-    password = "Boe888888"  # 密码
+    username = ""  # ssh 用户名
+    password = ""  # 密码
 
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
@@ -58,14 +54,11 @@ def upload_file(remote_path="/wang/save_model/ccks2020ee/", file_path="./README.
 
 def upload_file2(remote_path="/mnt/bd1/pubuser/wbq_data/", file_path="./"):
     # img_name示例：07670ff76fc14ab496b0dd411a33ac95-6.webp
-    # host = "10.10.150.91"  # 服务器ip地址
-    # port = 22  # 端口号
-    # username = "root"  # ssh 用户名
-    # password = "bd@2020"  # 密码
-    host = "10.10.177.28"  # 服务器ip地址
+
+    host = ""  # 服务器ip地址
     port = 22  # 端口号
-    username = "pubuser"  # ssh 用户名
-    password = "Boe888888"  # 密码
+    username = ""  # ssh 用户名
+    password = ""  # 密码
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
     ssh_client.connect(host, port, username, password)
